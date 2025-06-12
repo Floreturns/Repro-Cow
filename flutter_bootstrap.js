649394d19492c6c0dd1f35762e08569c0efc642a
Floreturns <Floreturns@users.noreply.github.com>
2025-06-12 14:33:22 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3184256285"
+    serviceWorkerVersion: "673650137"
   }
 });
